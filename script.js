a48dba5f71fda6702886c614dc10c64cb489a593
AdrienCappelaere <adrien.cappelaere@gmail.com>
2021-06-29 00:30:09 -0400

    Corrected the functions to correctly change the global functions in the local scope

--- script.js
+++ script.js
@@ -2,19 +2,28 @@
 
 
 */
+let computerChoice = "";
+let playerChoice = "";
 
+computerPlay(computerChoice);
+playerSelection(playerChoice);
+
+console.log("Computer choice: " + computerChoice + " and Player choice: " + playerChoice);
+
+playRound(computerChoice, playerChoice);
+
+// Function to define a random int in a given range
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Function to capitalize the first element of a string
 function capitalizeFirst(string) {
     return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
 
 function computerPlay() {
     let computerNumber = getRandomInt(3);
-    let computerChoice = "";
-
     if (computerNumber === 0) {
         computerChoice = "Rock";
     } else if (computerNumber === 1) {
@@ -22,28 +31,29 @@ function computerPlay() {
     } else {
         computerChoice = "Scissors";
     }
-    console.log(computerChoice);
+    console.log("Computer choice: " + computerChoice);
+    return computerChoice;
 }
 
 function playerSelection() {
-    let playerChoice = prompt("What do you want to play ? Choose between Rock, Paper and Scissors.");
+    playerChoice = prompt("What do you want to play ? Choose between Rock, Paper and Scissors.");
     playerChoice = capitalizeFirst(playerChoice);
-    console.log(playerChoice);
+    console.log("Player choice: " + playerChoice);
+    return playerChoice;
 }
 
 function playRound(computerChoice, playerChoice) {
+    console.log("Player choice: " + playerChoice);
+    console.log("Computer choice: " + computerChoice);
     if (computerChoice === playerChoice) {
         console.log("It's a tie !");
     } else if (
-        (computerChoice == "Rock" && playerChoice == "Scissors") || 
-        (computerChoice == "Scissors" && playerChoice == "Paper") || 
-        (computerChoice == "Paper" && playerChoice == "Rock")) {
+        (computerChoice === "Rock" && playerChoice === "Scissors") || 
+        (computerChoice === "Scissors" && playerChoice === "Paper") || 
+        (computerChoice === "Paper" && playerChoice === "Rock")) {
         console.log("The computer wins this round !");
     } else {
         console.log("You win this round !");
     }
 }
 
-computerPlay();
-playerSelection();
-playRound();
